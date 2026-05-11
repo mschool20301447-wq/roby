e24cd57e42066d5d01d77f751b59ac2868b99969
mschool20301447-wq <mschool2030.1447@gmail.com>
2026-05-11 07:47:55 +0300

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,5 @@
+basic.show_string("lubna")
+
+def on_forever():
+    basic.show_icon(IconNames.HEART)
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,4 @@
+basic.showString("lubna")
+basic.forever(function on_forever() {
+    basic.showIcon(IconNames.Heart)
+})
